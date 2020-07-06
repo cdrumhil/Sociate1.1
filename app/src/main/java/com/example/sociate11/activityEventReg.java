82e0316022e93c32062827c91e11c5567231e531
cdrumhil <chad.drumhiller@gmail.com>
2020-07-06 16:34:38 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sociate11/activityEventReg.java
@@ -0,0 +1,14 @@
+package com.example.sociate11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityEventReg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_reg);
+    }
+}
---
+++ app/src/main/java/com/example/sociate11/activityMyEvents.java
@@ -0,0 +1,14 @@
+package com.example.sociate11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityMyEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_events);
+    }
+}
